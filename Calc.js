25894a9cad3908170bd324a79449d3fe58383251
Homura-san <erickbispo@ufba.br>
2023-02-15 18:31:43 -0300

    Métodos estáticos

---
+++ Calc.js
@@ -0,0 +1,15 @@
+// Métodos estáticos
+
+class Calculadora{
+
+    static Somar(a, b){
+        console.log(a+b)
+    }
+
+    static Sub(a, b){
+        console.log(a-b)
+    }
+}
+
+Calculadora.Somar(10, 20)
+Calculadora.Sub(10, 520)
