9b28492c6f8eb749afd8a32fe5f2222a75d1cdef
Homura-san <erickbispo@ufba.br>
2023-02-15 18:17:32 -0300

    Função this

--- Filme.js
+++ Filme.js
@@ -23,11 +23,29 @@ class Filme{
     Fechar(){
         console.log('Fechar X')
     }
+
+    ficha(){
+        console.log("-----------Ficha-----------")
+        console.log("")
+        console.log("Título: " + this.titulo)
+        console.log("ano: " + this.ano)
+        console.log("Gênero: " + this.genero)
+        console.log("")
+
+        this.Reproduzir()
+    }
 }
 
+
 var vingadores = new Filme("Vingadores 2", 2014, 'Ação', 'fulano', '2h');
-console.log(vingadores.titulo, " ", vingadores.ano, " ", vingadores.genero)
+
+vingadores.ficha()
+
+//console.log(vingadores.titulo, " ", vingadores.ano, " ", vingadores.genero)
  
 var batman = new Filme("Batman", 2009, "Ação", "fulano 2", "1h");
-console.log(batman.titulo, " ", batman.ano, " ", batman.genero)
+
+batman.ficha()
+
+//console.log(batman.titulo, " ", batman.ano, " ", batman.genero)
 
